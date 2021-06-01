fbd1e51ade98d6d3d3ff457702e8a562810b50b6
dami-akinbi <akinbidamilola074@gmail.com>
2021-06-01 12:11:04 +0100

    discard first, my initial commit

---
+++ projects/area_calc.py
@@ -0,0 +1,13 @@
+# exercise
+# name = input('Tell me your name, buddy: ')
+# age = input('Tell me your age, please: ')
+#
+# print('Hi, ' + name + '. You are ' + age + ' at the moment.')
+
+
+# exercise - calculate the area of a circle
+radius = input('Enter the radius (m): ')
+area = 3.142 * float(radius) ** 2
+
+# print('The area of your circle is: ' + str(area))
+print('The area of your circle is:', round(area, 2))
